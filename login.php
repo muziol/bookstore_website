b55a327c7ee82252eceabc8db35213444952b792
muziol <bany.banan@gmail.com>
2017-11-02 23:57:30 +0100

    fix login.php

--- login.php
+++ login.php
@@ -7,11 +7,11 @@
 <body>
     <form action="checkLogin.php" method="get" class="log_in show">
         <label for="Email">
-            <input type="email" name="email" class="login_email" placeholder="email">
+            <input type="email" name="email" class="login_email" placeholder="e-mail">
         </label>
         
         <label for="Password">
-            <input type="password" name="password" class="login_password" placeholder="password">
+            <input type="password" name="password" class="login_password" placeholder="hasło">
         </label>
 
         <label for="submit">
@@ -27,16 +27,28 @@
     </form>
     
     <form action="sign_up.php" method="post" class="sign_up hide">
+        <label for="Name">
+            <input type="text" name="name" class="sign_name" placeholder="imię">
+        </label>
+
+        <label for="Surname">
+            <input type="text" name="surrname" class="sign_surname" placeholder="nazwisko">
+        </label>
+
         <label for="Email">
-            <input type="email" name="email" class="sign_email" placeholder="email">
+            <input type="email" name="email" class="sign_email" placeholder="e-mail">
         </label>
 
         <label for="Password">
-            <input type="password" name="password" class="sign_password" placeholder="password">
+            <input type="password" name="password" class="sign_password" placeholder="hasło">
         </label>
 
-        <label for="Replay_password">
-            <input type="password" name="replay_password" class="sign_replay_password" placeholder="replay password">
+        <label for="Repeat_password">
+            <input type="password" name="repeat_password" class="sign_repeat_password" placeholder="powtórz hasło">
+        </label>
+
+        <label for="phone">
+            <input type="tel" name="phone" class="sing_phone" placeholder="numer telefonu">
         </label>
 
         <label for="submit">
