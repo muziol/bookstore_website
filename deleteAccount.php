b0baaf54709db8064b2956b39e7dbe06f5927a25
Th3NiKo <niko.pachala@onet.eu>
2018-01-30 15:35:43 +0100

    push

--- deleteAccount.php
+++ deleteAccount.php
@@ -7,11 +7,12 @@ if(!isset($_COOKIE['token'])) {header('Location: log_in.php');}
 $url =  $_SESSION['apiIP'].'/user'.'/delete';
    
 $ch = curl_init($url);        
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");    
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
     "Authorization: Bearer ".$_COOKIE['token'])
 );
 $result = curl_exec($ch);
-header("Location: index.php");
+header("Location: logout.php");
 
 
 ?>
